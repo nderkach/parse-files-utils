5c3c89ed7c9645635c240a77ec07de36472d0f18
Nikolay Derkach <nderk@me.com>
2016-10-19 19:37:09 -0700

    port parsefss links

--- config.example.js
+++ config.example.js
@@ -3,40 +3,20 @@ var S3Adapter = require('parse-server-s3-adapter');
 var GCSAdapter = require('parse-server-gcs-adapter');
 
 module.exports = {
-  applicationId: "PARSE_APPLICATION_ID",
-  masterKey: "PARSE_MASTER_KEY",
-  mongoURL: "mongodb://<username>:<password>@mongourl.com:27017/database_name",
-  serverURL: "https://api.customparseserver.com/parse",
-  filesToTransfer: 'parseOnly',
-  renameInDatabase: false,
-  transferTo: 'filesystem',
+  applicationId: "STUB",
+  masterKey: "STUB",
+  mongoURL: "STUB",
+  serverURL: "https://api.parse.com/1",
+  filesToTransfer: 'all',
+  renameInDatabase: true,
+  renameFiles: true,
+  transferTo: 's3',
 
   // For filesystem configuration
   filesystemPath: './downloaded_files',
 
   // For S3 configuration
-  aws_accessKeyId: "ACCESS_KEY_ID",
-  aws_secretAccessKey: "SECRET_ACCESS_KEY",
-  aws_bucket: "BUCKET_NAME",
-  aws_bucketPrefix: "",
-
-  // For GCS configuration
-  gcs_projectId: "GCS_PROJECT_ID",
-  gcs_keyFilename: "credentials.json",
-  gcs_bucket: "BUCKET_NAME",
-
-  // Or set filesAdapter to a Parse Server file adapter
-  // filesAdapter: new FileAdapter({
-  //  filesSubDirectory: './downloaded_files'
-  // }),
-  // filesAdapter: new S3Adapter({
-  //   accessKey: 'ACCESS_KEY_ID',
-  //   secretKey: 'SECRET_ACCESS_KEY',
-  //   bucket: 'BUCKET_NAME'
-  // }),
-  // filesAdapter: new GCSAdapter({
-  //   projectId: "GCS_PROJECT_ID",
-  //   keyFilename: "credentials.json",
-  //   bucket: "BUCKET_NAME",
-  // }),
+  aws_accessKeyId: "STUB",
+  aws_secretAccessKey: "STUB",
+  aws_bucket: "STUB",
 };
